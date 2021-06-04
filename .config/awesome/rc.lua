365faa2bfb984db67aafaad94c3e381e4649f36d
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2021-06-04 15:52:25 +0200

    changed polybar theme name

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -433,5 +433,5 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 -- Autostart
 awful.spawn.with_shell("picom --experimental-backends")
-awful.spawn.with_shell("/home/alejandro/.config/polybar/launch.sh --shapes")
+awful.spawn.with_shell("/home/alejandro/.config/polybar/forest/launch.sh")
 awful.spawn(terminal.." -e dijo")
