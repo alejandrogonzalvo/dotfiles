bb78cb1adc3ed385ea575efb4e6a86d5fbb9ed16
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2021-09-29 08:53:54 +0200

    clean trash

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -474,6 +474,10 @@ awful.rules.rules = {
     -- Solves Firefox buggy behaviour
     { rule = { class = "firefox" },
       properties = { opacity = 1, maximized = false, floating = false } },
+      
+    -- Solves Nautilus buggy behaviour
+    { rule = { class = "Nautilus" },
+      properties = { opacity = 1, maximized = false, floating = false } },
 
     { rule = { class = "mpv" },
       properties = {border_width = 0 }},
@@ -487,6 +491,9 @@ client.connect_signal("manage", function (c)
     -- Set the windows at the slave,
     -- i.e. put it at the end of others instead of setting it master.
     -- if not awesome.startup then awful.client.setslave(c) end
+    c.shape = function(cr, w, h)
+      gears.shape.rounded_rect(cr, w, h, 4)
+    end
     if awesome.startup
       and not c.size_hints.user_position
       and not c.size_hints.program_position then
